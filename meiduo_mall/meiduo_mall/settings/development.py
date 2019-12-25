3a93642faf75c9f08a379491ca7d2c1f8aff333b
Aaron <troublemaker.5354@gmail.com>
2019-12-25 12:02:25 +0800

    setup jinja2 template

--- meiduo_mall/meiduo_mall/settings/development.py
+++ meiduo_mall/meiduo_mall/settings/development.py
@@ -53,10 +53,12 @@ ROOT_URLCONF = 'meiduo_mall.urls'
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        # 'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'BACKEND': 'django.template.backends.jinja2.Jinja2',
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
+            'environment': 'meiduo_mall.utils.jinja2_environment.jinja2_env',
             'context_processors': [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
