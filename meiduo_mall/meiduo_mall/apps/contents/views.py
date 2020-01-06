cfde7188f0e73ac75aeb490dc88bb103392ffaac
Aaron <troublemaker.5354@gmail.com>
2020-01-06 08:27:14 +0800

    GoodsChannelGroup without create_time and update_time problem fixed

--- meiduo_mall/meiduo_mall/apps/contents/views.py
+++ meiduo_mall/meiduo_mall/apps/contents/views.py
@@ -1,12 +1,21 @@
 from django.shortcuts import render
-from django.views import View
 from django import http
+
+from goods.models import GoodsChannelGroup
 
 
 def index(request):
     """ 主页 """
     if request.method == 'GET':
 
-        return render(request, 'index.html')
+        context = {}
+        goods_channel_group = []
+
+        for group in GoodsChannelGroup.objects.all():
+            goods_channel_group.append(group)
+
+        context['goods_channel_group'] = goods_channel_group
+
+        return render(request, 'index.html', context)
     else:
         return http.HttpResponseForbidden()
